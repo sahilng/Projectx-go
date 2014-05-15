a98baad59fd505e3675c59d629674e699273443c
sahilng <sahilng1997@gmail.com>
2014-05-14 23:15:04 -0400

    first commit

---
+++ hello.go
@@ -0,0 +1,31 @@
+package main
+
+import(
+    "net/http"
+    "log"
+    "os"
+)
+
+const resp = `<html>
+    <head>
+        <title>ProjectX</title>
+    </head>
+    <body>
+        <h1>ProjectX's Dev Site</h1>
+        <p>Hello World!</p>
+    </body>
+</html>`
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte(resp))
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    err := http.ListenAndServe(":80", nil)
+
+    if err != nil {
+        log.Println(err)
+        os.Exit(1)
+    }
+}
