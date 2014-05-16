5de34c966a93cb92cd7cbbc74b0affbf35372449
sahilng <sahilng1997@gmail.com>
2014-05-15 22:14:36 -0400

    sadf

--- hello.go
+++ hello.go
@@ -1,36 +1,15 @@
 package main
 
-import(
+import (
+    "fmt"
     "net/http"
-    "log"
-    "os"
 )
 
-const resp = `<html>
-    <head>
-        <title>ProjectX</title>
-        <link rel=stylehseet type=text href="static/style.css">
-    </head>
-    <body>
-        <h1>ProjectXs Dev Site</h1>
-        <p>Hello World.</p>
-    </body>
-</html>`
-
-func handler(w http.ResponseWriter, r *http.Request) {
-    w.Write([]byte(resp))
+func main() {
+    http.HandleFunc("/", homeHandler)
+    panic(http.ListenAndServe(":17901", nil))
 }
 
-
-func main() {
-    http.HandleFunc("/", handler)
-
-    http.Handle("/static", http.FileServer(http.Dir("./static/")))
-
-    err := http.ListenAndServe(":80", nil)
-
-    if err != nil {
-        log.Println(err)
-        os.Exit(1)
-    }
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "<html><head></head><body><h1>Welcome Home!</h1></body></html>")
 }
